27c0b96c9a165bbf05bb6f7d42a0bc462943b25c
Prashant Verma <prashant.verma@smartbear.com>
2020-12-14 18:24:52 +0530

    CC-23805: Implemented callable to support parent-child structure in Jenkins

--- src/main/java/com/thed/service/BaseService.java
+++ src/main/java/com/thed/service/BaseService.java
@@ -1,7 +1,9 @@
 package com.thed.service;
 
+import java.io.Serializable;
+
 /**
  * Created by prashant on 25/6/19.
  */
-public interface BaseService {
+public interface BaseService extends Serializable {
 }
